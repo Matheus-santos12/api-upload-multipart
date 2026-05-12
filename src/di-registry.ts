3eabd59151ef1de15aa749bb5cc91a72057eb437
Matheus Silva <rematheussilva@gmail.com>
2026-05-12 16:58:10 -0500

    feat: add storage switch between s3 and local via env variable

--- src/di-registry.ts
+++ src/di-registry.ts
@@ -1,8 +1,12 @@
 import { injectablesHolder } from "fastify-decorators";
-// import { LocalStorage } from "./storage/local-storage.js";
+import { LocalStorage } from "./storage/local-storage.js";
 import { S3Storage } from "./storage/s3-storage.js";
 import { StorageProvider } from "./storage/storage.js";
 
 export function registerDependencies(): void {
-  injectablesHolder.injectService(StorageProvider, S3Storage);
+  if (process.env.STORAGE === "s3") {
+    injectablesHolder.injectService(StorageProvider, S3Storage);
+  } else {
+    injectablesHolder.injectService(StorageProvider, LocalStorage);
+  }
 }
